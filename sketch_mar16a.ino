0310f7c2ec833973d979edc8275020868dda242c
Mateusz Niecikowski <Mateusz.Niecikowski@unit4.com>
2024-03-24 15:04:53 +0100

    refactoring

--- sketch_mar16a.ino
+++ sketch_mar16a.ino
@@ -6,6 +6,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <Toggle.h>
+#include <U8g2lib.h>
+
 
 #define DEBUG_BAUDIOS 9600
 #define POTENTIOMETER_PIN A0
@@ -17,48 +19,41 @@
 #define SPEED_CONST 14
 #define FAST_SPEED 100
 
-
-#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET -1   //   QT-PY / XIAO
-
+#define i2c_Address 0x3c 
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1   
 
 #define NUMFLAKES 10
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
 
-
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
-
 
 constexpr int START_BUTTON_PIN = 12;
 constexpr int STOP_BUTTON_PIN = 11;
 constexpr int LEFT_SWITCH_PIN = 10;
 constexpr int RIGHT_SWITCH_PIN = 9;
 
-// defines pins numbers
 const int stepPin = 3;
 const int directionPin = 2;
 const int enablePin = 4;
 
 float speed = 23;
-float direction = 0;
+float direction = 1;
 
 bool stepperRun = false;
 bool fastRunForward = false;
 bool fastRunBackward = false;
 
+const int ledPin = 13;    // the number of the LED pin
+
 static const unsigned long REFRESH_INTERVAL = 100;
 
 static unsigned long lastRefreshTime = 0;
 
-//using namespace smartbutton;
-
-// Define a stepper and the pins it will use
-// 1 or AccelStepper::DRIVER means a stepper driver (with Step and Direction pins)
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, directionPin);
 
 MagicPot potentiometer(POTENTIOMETER_PIN, POTENTIOMETER_MIN_READ, POTENTIOMETER_MAX_READ);
@@ -80,124 +75,54 @@ void eventSwitchRightCallback(SmartButton *button, SmartButton::Event event, int
       direction = 0;
     }
 }
-void onPressedForDuration()
-{
-  fastRunForward = true;  
-}
-void onPressed()
-{
-  stepperRun = true;
-}
-
-void eventStartCallback(SmartButton *button, SmartButton::Event event, int clickCounter)
-{
-    if (event == SmartButton::Event::CLICK) {       
-      stepperRun = true;
-    } else if (event == SmartButton::Event::HOLD) {     
-      fastRunForward = true;  
-    } else if (event == SmartButton::Event::RELEASED) {
-      fastRunForward = false;
-    }
-}
-
-void eventStopCallback(SmartButton *button, SmartButton::Event event, int clickCounter)
-{
-    if (event == SmartButton::Event::CLICK) {       
-      stepperRun = false;
-    } else if (event == SmartButton::Event::HOLD) {    
-      fastRunBackward = true;     
-    } else if (event == SmartButton::Event::RELEASED) {  
-      fastRunBackward = false;  
-    }
-}
 */
-Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+//Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+Toggle startButton(START_BUTTON_PIN);
+Toggle stopButton(STOP_BUTTON_PIN);
+Toggle directionSwitch(LEFT_SWITCH_PIN, RIGHT_SWITCH_PIN);
 
 void draw() {
-  
-  // text display tests
-  display.setCursor(0, 0);
-  display.setTextSize(1);
+  u8g2.clearBuffer();				
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+
+  char resultS[8];
+  u8g2.drawStr(0,10,"S:");	// write something to the internal memory
+  dtostrf(speed, 6, 2, resultS);
+  u8g2.drawStr(50,10,resultS);	// write something to the internal memory
+
+  char resultP[8];
+  u8g2.drawStr(0,20,"P:");	// write something to the internal memory
+  dtostrf(speed * 0.3, 6, 2, resultP);
+  u8g2.drawStr(50,20,resultP);	// write something to the internal memory
+
+  char resultD[8];
+  u8g2.drawStr(0,30,"D:");	// write something to the internal memory
+  dtostrf(direction, 6, 2, resultD);
+  u8g2.drawStr(50,30,resultD);	// write something to the internal memory
+
+  char resultSCP[8];
+  u8g2.drawStr(0,40,"SCP:");	// write something to the internal memory
+  dtostrf(stepper.currentPosition(), 6, 2, resultSCP);
+  u8g2.drawStr(50,40,resultSCP);	// write something to the internal memory
+
+  u8g2.sendBuffer();		
+
+  /*display.setCursor(0, 0);
+  display.setTextSize(2);
   display.setTextColor(SH110X_WHITE);
   display.print("S: "); display.println(speed);
   display.print("P: "); display.print(speed * 0.3); display.println("mm");
   display.print("D: "); display.println(direction);
-  display.print("FRF: "); display.println(fastRunForward);
-  display.print("SR: "); display.println(stepperRun);
+  display.print("SCP: "); display.println(stepper.currentPosition());
   display.display();
-  //delay(2000);
-  display.clearDisplay();
+  display.clearDisplay();*/
 }
 
-Toggle startButton(START_BUTTON_PIN);
-
-//EasyButton startButton(START_BUTTON_PIN);
-/*SmartButton startButton(START_BUTTON_PIN, SmartButton::InputType::NORMAL_HIGH);
-SmartButton stopButton(STOP_BUTTON_PIN, SmartButton::InputType::NORMAL_HIGH);
-
-SmartButton leftSwitch(LEFT_SWITCH_PIN, SmartButton::InputType::NORMAL_HIGH);
-SmartButton rightSwitch(RIGHT_SWITCH_PIN, SmartButton::InputType::NORMAL_HIGH);
-*/
-void setup()
-{
-	//Serial.begin(DEBUG_BAUDIOS);
-  Serial.begin(9600);
-
-  delay(250); // wait for the OLED to power up
-  display.begin(i2c_Address, true); // Address 0x3C default
- //display.setContrast (0); // dim display
- 
-  display.display();
-  delay(2000);
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  /*pinMode(START_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(STOP_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LEFT_SWITCH_PIN, INPUT_PULLUP);
-  pinMode(RIGHT_SWITCH_PIN, INPUT_PULLUP);
-
-  startButton.begin(eventStartCallback);
-  stopButton.begin(eventStopCallback);
-  leftSwitch.begin(eventSwitchLeftCallback);
-  rightSwitch.begin(eventSwitchRightCallback);
-*/
-  //startButton.begin();
-  // Add the callback function to be called when the button is pressed for at least the given time.
-  //startButton.onPressedFor(1000, onPressedForDuration);
-  //startButton.onPressed(onPressed);
-
-	potentiometer.begin();
-
-  stepper.setPinsInverted(false, false, true);
-  stepper.setEnablePin(enablePin);
-  stepper.enableOutputs();
-  stepper.setMaxSpeed(60000);
-  
-  
-  startButton.begin(START_BUTTON_PIN);
-}
-
-void loop()
-{
-  startButton.poll();
-
-  if(startButton.pressedFor(1000)){
-    fastRunForward = true; 
-  }
-  
-  if(startButton.isPressed()){
-    stepperRun = true;
-  }
-
-  //startButton.read();
-  //SmartButton::service();  
-	potentiometer.read();
-
-  speed = (((MAX_SPEED - MIN_SPEED) * potentiometer.getValue()) / POTENTIOMETER_MAX_READ) + MIN_SPEED;
-
+void updateStepper(){
   if (fastRunForward){
       stepper.setSpeed(SPEED_CONST * FAST_SPEED * direction);
       stepper.runSpeed();
@@ -210,11 +135,85 @@ void loop()
   } else {
     stepper.stop();
   }
+}
+
+void calcSpeed(){
+  speed = (((MAX_SPEED - MIN_SPEED) * potentiometer.getValue()) / POTENTIOMETER_MAX_READ) + MIN_SPEED;
+}
+
+void updateServices(){
+  startButton.poll();
+  stopButton.poll();
+  directionSwitch.poll();
   
+	potentiometer.read();
+}
+
+void readButtons(){
+  if (startButton.pressedFor(1000)){
+    fastRunForward = true; 
+  } else if (startButton.isPressed()){
+    stepperRun = true;
+  } else if (startButton.isReleased()){
+    fastRunForward = false;
+  }
+
+  if (stopButton.pressedFor(1000)){
+    fastRunBackward = true; 
+  } else if (stopButton.isPressed()){
+    stepperRun = false;
+  } else if (stopButton.isReleased()){
+    fastRunBackward = false;
+  }
+  
+  if (directionSwitch.isMID()) {
+    direction = 0;
+  } else if (directionSwitch.isUP()) {
+    direction = 1;
+  } else if (directionSwitch.isDN()) {
+    direction = -1;
+  }
+}
+
+void sendOutput(){
   if(millis() - lastRefreshTime >= REFRESH_INTERVAL)
 	{
     draw();
 		lastRefreshTime += REFRESH_INTERVAL;
 	  //Serial.println(String("S: ") + speed + String(", D: ") + direction + String(", FRF: ") + fastRunForward + String(", FRB: ") + fastRunBackward + String(", SR: ") + stepperRun + String(", SP: ") + stepper.currentPosition() + String(", SS: ") + stepper.speed() + String(", MS: ") + stepper.maxSpeed());
 	}
+}
+
+void setup()
+{
+	Serial.begin(DEBUG_BAUDIOS);
+  /*delay(250);
+  display.begin(i2c_Address, true);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+*/
+
+  u8g2.begin();
+
+	potentiometer.begin();
+
+  stepper.setPinsInverted(false, false, true);
+  stepper.setEnablePin(enablePin);
+  stepper.enableOutputs();
+  stepper.setMaxSpeed(60000);
+  
+  //pinMode(ledPin, OUTPUT);
+
+  startButton.begin(START_BUTTON_PIN);
+  stopButton.begin(STOP_BUTTON_PIN);
+  directionSwitch.begin(LEFT_SWITCH_PIN, RIGHT_SWITCH_PIN);
+}
+
+void loop()
+{
+  updateServices();
+  calcSpeed();
+  updateStepper();
+  sendOutput();
 }
